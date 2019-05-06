5231c861e6e8724805bd035a3de7facf6837c31e
Corey Castillo <mcguiser@users.noreply.github.com>
2019-05-06 12:57:34 -0500

    Created gatsby project. Added coming soon message and links

---
+++ README.md
@@ -0,0 +1,2 @@
+<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+<a href="https://www.coreycastillo.dev">Portfolio Link</a>
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default () => 
+    <div style={{margin : 'auto', width : '60%', textAlign : 'center', position : 'relative', top : '50%', transform : 'translateY(50%)'}}>
+        <div>
+            <h1 style={{fontFamily : 'Segoe UI'}}>Coming soon...</h1>
+        </div>
+        <div>
+            <h3 style={{fontFamily : 'Palatino Linotype'}}>In the meantime check out the following links to get to know Corey:</h3>
+            <p>
+                <table style={{margin : '0 auto', borderSpacing : '15px 0'}}>
+                    <tr>
+                        <td><a href="https://s3-us-west-1.amazonaws.com/coreycastillo.dev/resume.pdf">Resume</a></td>
+                        <td><a href="https://github.com/mcguiser">Github</a></td>
+                        <td><a href="https://www.linkedin.com/in/corey-castillo/">LinkedIn</a></td>
+                    </tr>
+                </table>
+            </p>
+            <p>
+                <h4 style={{fontFamily : 'Palatino Linotype'}}>Send me an email: contact@coreycastillo.dev</h4>
+            </p>
+        </div>
+    </div>
